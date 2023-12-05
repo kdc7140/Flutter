d49707809fe8c8413d972d03cf03136ebdc802e1
kdc7140 <kdc7140@naver.com>
2023-12-06 00:33:48 +0900

    add example - image_carousel

---
+++ image_carousel/lib/screen/home_screen.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+  PageController controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    timer = Timer.periodic(Duration(seconds: 4), (timer) {
+      int currentPage = controller.page!.toInt();
+      int nextPage = currentPage + 1;
+
+      if (nextPage > 4) {
+        nextPage = 0;
+      }
+
+      controller.animateToPage(
+        nextPage,
+        duration: Duration(milliseconds: 400),
+        curve: Curves.linear,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    if (timer != null) {
+      timer!.cancel();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //상태바 (시계 등) 색상 변경
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+
+    return Scaffold(
+        body: PageView(
+      controller: controller,
+      children: [1, 2, 3, 4, 5]
+          .map(
+            (e) => Image.asset(
+              'asset/img/image_$e.jpeg',
+              fit: BoxFit.cover,
+            ),
+          )
+          .toList(),
+      // children: [
+      //   Image.asset('asset/img/image_1.jpeg', fit: BoxFit.cover),
+      //   Image.asset('asset/img/image_2.jpeg'),
+      //   Image.asset('asset/img/image_3.jpeg'),
+      //   Image.asset('asset/img/image_4.jpeg'),
+      //   Image.asset('asset/img/image_5.jpeg'),
+      // ],
+    ));
+  }
+}

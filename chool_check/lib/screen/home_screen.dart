03d928e7052c06d00638b256b32900c2e81b74cc
kdc7140 <kdc7140@naver.com>
2023-12-20 22:57:41 +0900

    example - chool check(in grogress - viewed marker)

--- chool_check/lib/screen/home_screen.dart
+++ chool_check/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,27 +10,79 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  static final LatLng companyLatLng = LatLng(37.5233273, 126.921252);
+  static final LatLng companyLatLng =
+      LatLng(37.47123863176653, 126.88927401682312);
   static final CameraPosition initialPosition =
       CameraPosition(target: companyLatLng, zoom: 15);
+
+  static final double distance = 100;
+  static final Circle circle = Circle(
+    circleId: CircleId('circle'),
+    center: companyLatLng,
+    fillColor: Colors.blue.withOpacity(0.5),
+    radius: distance,
+    strokeColor: Colors.blue,
+    strokeWidth: 1,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: renderAppBar(),
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          _CustomGoogleMap(
-            initialPosition: initialPosition,
-          ),
-          _ChoolCheckButton(),
-        ],
-      ),
-    );
+        appBar: renderAppBar(),
+        backgroundColor: Colors.white,
+        body: FutureBuilder(
+          future: checkPermission(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (snapshot.data == "위치 권한이 허가 되었습니다.") {
+              return Column(
+                children: [
+                  _CustomGoogleMap(
+                    initialPosition: initialPosition,
+                    circle: circle,
+                  ),
+                  _ChoolCheckButton(),
+                ],
+              );
+            }
+
+            return Center(
+              child: Text(snapshot.data),
+            );
+          },
+        ));
   }
 
-  AppBar renderAppBar(){
+  Future<String> checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!isLocationEnabled) {
+      return '위치 서비스를 활성화 해주세요.';
+    }
+
+    LocationPermission checkPermission = await Geolocator.checkPermission();
+
+    if (checkPermission == LocationPermission.denied) {
+      checkPermission = await Geolocator.requestPermission();
+
+      if (checkPermission == LocationPermission.denied) {
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+
+    if (checkPermission == LocationPermission.deniedForever) {
+      return '앱의 위치 권한을 세팅에서 허가해주세요.';
+    }
+
+    return '위치 권한이 허가 되었습니다.';
+  }
+
+  AppBar renderAppBar() {
     return AppBar(
       title: Text(
         '출근 또 해?',
@@ -44,8 +97,13 @@ class _HomeScreenState extends State<HomeScreen> {
 
 class _CustomGoogleMap extends StatelessWidget {
   final CameraPosition initialPosition;
+  final Circle circle;
 
-  const _CustomGoogleMap({required this.initialPosition, super.key});
+  const _CustomGoogleMap({
+    required this.initialPosition,
+    required this.circle,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -54,6 +112,9 @@ class _CustomGoogleMap extends StatelessWidget {
       child: GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: initialPosition,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: false,
+        circles: Set.from([circle]),
       ),
     );
   }
@@ -67,5 +128,3 @@ class _ChoolCheckButton extends StatelessWidget {
     return Expanded(child: Text('출근'));
   }
 }
-
-

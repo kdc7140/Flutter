e067b4fa0577edf55d756ea93189ac9de5e658b3
kdc7140 <kdc7140@naver.com>
2024-01-14 00:12:53 +0900

    exmaple - calendar scheduler(add language & banner)

--- calendar_scheduler/lib/component/calendar.dart
+++ calendar_scheduler/lib/component/calendar.dart
@@ -1,20 +1,33 @@
+import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatefulWidget {
-  const Calendar({super.key});
+class Calendar extends StatelessWidget{
+  final DateTime? selectedDay;
+  final DateTime focusedDay;
+  final OnDaySelected? onDaySelected;
 
-  @override
-  State<Calendar> createState() => _CalendarState();
-}
-
-class _CalendarState extends State<Calendar> {
-  DateTime? selectedDay;
+  const Calendar({
+    required this.selectedDay,
+    required this.focusedDay,
+    required this.onDaySelected,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final defaultBoxDeco = BoxDecoration(
+      borderRadius: BorderRadius.circular(6.0),
+      color: Colors.grey[200],
+    );
+    final defaultTextStyle = TextStyle(
+      color: Colors.grey[600],
+      fontWeight: FontWeight.w700,
+    );
+
     return TableCalendar(
-      focusedDay: DateTime.now(),
+      locale: 'ko_KR',
+      focusedDay: focusedDay,
       firstDay: DateTime(1800),
       lastDay: DateTime(3000),
       headerStyle: HeaderStyle(
@@ -25,16 +38,33 @@ class _CalendarState extends State<Calendar> {
           fontSize: 16.0,
         ),
       ),
-      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
-        print(selectedDay);
-        setState(() {
-          this.selectedDay = selectedDay;
-        });
-      },
+      calendarStyle: CalendarStyle(
+        isTodayHighlighted: false,
+        defaultDecoration: defaultBoxDeco,
+        weekendDecoration: defaultBoxDeco,
+        selectedDecoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(6.0),
+          border: Border.all(
+            color: PRIMARY_COLOR,
+            width: 1.0,
+          ),
+        ),
+        outsideDecoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+        ),
+        defaultTextStyle: defaultTextStyle,
+        weekendTextStyle: defaultTextStyle,
+        selectedTextStyle: defaultTextStyle.copyWith(
+          color: PRIMARY_COLOR,
+        ),
+      ),
+      onDaySelected: onDaySelected,
       selectedDayPredicate: (DateTime date) {
-        if(selectedDay == null){
+        if (selectedDay == null) {
           return false;
         }
+
         return date.year == selectedDay!.year &&
             date.month == selectedDay!.month &&
             date.day == selectedDay!.day;
--- calendar_scheduler/lib/screen/home_screen.dart
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -1,8 +1,21 @@
+import 'package:calendar_scheduler/component/calendar.dart';
+import 'package:calendar_scheduler/component/today_banner.dart';
 import 'package:flutter/material.dart';
-import '../component/calendar.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDay = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+  DateTime focusedDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +23,27 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Calendar(),
-          ]
+            Calendar(
+              selectedDay: selectedDay,
+              focusedDay: focusedDay,
+              onDaySelected: onDaySelected,
+            ),
+            SizedBox(height: 8.0),
+            TodayBanner(
+              selectedDay: selectedDay,
+              scheduleCount: 3,
+            ),
+            SizedBox(height: 8.0),
+          ],
         ),
       ),
     );
+  }
+
+  onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      this.selectedDay = selectedDay;
+      this.focusedDay = selectedDay;
+    });
   }
 }
